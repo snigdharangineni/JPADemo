97ead6ebca7fcc928594d4aa373af52734ff4210
snigdha <sunny.rangineni@gmail.com>
2024-06-05 20:16:59 -0500

    Connecting to Postgres with JPA

---
+++ src/main/java/com/jpa/JPADemo/App.java
@@ -0,0 +1,39 @@
+package com.jpa.JPADemo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+//    	Configuration con = new Configuration().configure().addAnnotatedClass(Alien.class);
+//    	
+//    	SessionFactory sf = con.buildSessionFactory();
+//    	Session session = sf.openSession();
+//    	session.beginTransaction();
+//    	
+//    	Alien alien = new Alien();
+//    	alien.setAid(101);
+//    	alien.setAname("Ram");
+//    	alien.setTech("Java");
+//    	
+//    	session.save(alien);
+//    	
+//    	session.getTransaction().commit();
+    	EntityManagerFactory emf = Persistence.createEntityManagerFactory("pu");
+    	EntityManager em = emf.createEntityManager();
+    	
+    	Alien alien = em.find(Alien.class, 101);
+    	System.out.println(alien);
+    }
+}
---
+++ src/test/java/com/jpa/JPADemo/AppTest.java
@@ -0,0 +1,20 @@
+package com.jpa.JPADemo;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
